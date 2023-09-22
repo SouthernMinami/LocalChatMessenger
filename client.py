6d5299420b218c9a1ef0b7a138376d6fc2a92fbc
vboxuser <vboxuser@Linux-Ubuntu.myguest.virtualbox.org>
2023-09-22 22:17:32 +0900

    [Fix] Modify client.py and server.py

--- client.py
+++ client.py
@@ -1,26 +1,30 @@
 import socket
 import os
 
+# create a UDP socket
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 server_address = '/udp_socket_file'
 address = '/udp_client_socket_file'
 
-message = input('Enter message: ').encode()
+message = input('Type "msg" to get a random message: ').encode()
 
+# if there is alredy a client address file, delete it
 try:
     os.unlink(address)
 except FileNotFoundError:
     pass
 
+# bind the socket to the server address
 sock.bind(address)
 
 try:
+    # send input message
     print('sending {!r}'.format(message))
     sent = sock.sendto(message, server_address)
 
     print('waiting to receive')
+    # receive response
     data, server = sock.recvfrom(4096)
-    
     print('received {!r}'.format(data))
 
 finally:
--- server.py
+++ server.py
@@ -2,9 +2,11 @@ import socket
 import os
 from faker import Faker
 
+# create a UDP socket
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 server_address = '/udp_socket_file'
 
+# if there is alredy a server address file, delete it
 try:
     os.unlink(server_address)
 except FileNotFoundError:
@@ -12,17 +14,21 @@ except FileNotFoundError:
 
 print('starting up on {}'.format(server_address))
 
+# bind the socket to the client address
 sock.bind(server_address)
 
 while True:
     print('\nwaiting to receive message')
+    # receive message
     data, address = sock.recvfrom(4096)
 
     if data:
         fake = Faker()
-        fakeMsg = fake.text().encode()
+        fakeMsg =  fake.text().encode() if data == b'msg' else b'Invalid message'
+        # send response random fake message
         sent = sock.sendto(fakeMsg, address)
         print('sent back the message {}'.format(fakeMsg))
 
+    # close the socket
     print('closing socket')
     sock.close()
